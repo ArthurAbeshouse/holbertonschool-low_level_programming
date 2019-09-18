eef39bd720a1a2f338417614343747236b4321e5
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-18 15:27:31 -0400

    print and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - uses printf to display message
- *
- * Return: Always 0 (Success)
- */
+* main - uses "write" to display message
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
