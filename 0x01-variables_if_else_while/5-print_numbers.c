2836fc91ae2f4a8e352c7517c266027446d932e6
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-19 10:38:16 -0400

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - uses "putschar" to print the english alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	return (0);
+}
