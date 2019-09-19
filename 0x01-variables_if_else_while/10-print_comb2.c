b1632993a39a24920472d491282de5f0ff20f937
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-19 15:42:19 -0400

    prints the numbers from 00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,8 +11,8 @@ int main(void)
 
 	for (n = 0; n < 100; n++)
 	{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 
 		if (n != 99)
 		{
