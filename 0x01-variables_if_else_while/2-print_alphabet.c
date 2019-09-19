74b2fb2c96b3e91e0073ff8d3bab9d6c6f2bcdbd
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-19 13:14:53 -0400

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - uses "putschar" to print the english alphabet in lowercase
 *
@@ -11,6 +12,10 @@ int main(void)
 	char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
+	putchar('\n');
+
 	return (0);
 }
