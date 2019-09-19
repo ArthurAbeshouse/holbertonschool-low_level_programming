86779b0826067748b61dfa08e7f2397968de8928
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-19 15:38:52 -0400

    prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the numbers from 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
