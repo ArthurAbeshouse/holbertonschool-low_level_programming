1971bbfce8c5ea3f1bd89b7e4a05554187ae4d50
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-23 15:11:52 -0400

    prints Holberton using predefined functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <holberton.h>
+/**
+ * main - Entry Point. Uses only user defined variables to create output
+ *
+ * Description: Prints Holberton using only predefined functions
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
