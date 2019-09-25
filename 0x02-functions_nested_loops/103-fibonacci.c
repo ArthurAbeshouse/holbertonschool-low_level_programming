2caf025622891cc65a299e861e06c2d259cbc705
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-24 21:47:51 -0400

    fibobnachi numbers starting from one

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+/**
+ * main - sum all even numbers under
+ * 4000000 into a fibobnachi sequence
+ * Description: fibobnachi numbers starting from one
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int fib1 = 0;
+	long int fib2 = 0;
+	long int fib3 = 0;
+	int counter = 0;
+
+	while (fib2 < 4000000)
+	{
+		fib2 = fib1 + fib3;
+		fib1 = fib3;
+		fib3 = fib2;
+		if (fib2 % 2 == 0)
+		{
+			sum = sum + fib2;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
