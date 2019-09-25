832ed645dea030c3ca0b00274dca566b88d35f61
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-24 22:14:35 -0400

    prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	long fib1, fib1a, fib1b, fib2, fib2a, fib2b, fib3, fib3a, fib3b;
-	long divi = 10000000000;
+	long n = 10000000000;
 
 	fib1 = 0;
 	fib2 = 1;
@@ -18,22 +18,22 @@ int main(void)
 
 	for (counter = 0; counter < 91; counter++)
 	{
-		fib3 =  fib2 + fib1;
+		fib3 = fib2 + fib1;
 		fib1 = fib2;
 		fib2 = fib3;
 		printf("%ld, ", fib3);
 	}
-	fib1a = fib1 / divi;
-	fib1b = fib1 % divi;
-	fib2a = fib2 / divi;
-	fib2b = fib2 % divi;
+	fib1a = fib1 / n;
+	fib1b = fib1 % n;
+	fib2a = fib2 / n;
+	fib2b = fib2 % n;
 	for (; counter < 98; counter++)
 	{
 		fib3a = fib1a + fib2a;
 		fib3b = fib1b + fib2b;
-		if (fib3b > divi)
+		if (fib3b > n)
 		{
-			fib3b = fib3b % divi;
+			fib3b = fib3b % n;
 			fib3a++;
 		}
 		if (counter != 97)
