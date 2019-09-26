7edd5f38e7f222111b58d15d7d1c9d09c8b22eb3
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-09-26 09:40:18 -0400

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
+ */
+void print_numbers(void)
+{
+	int n;
+	n = 0;
+	while (n <= 9)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
