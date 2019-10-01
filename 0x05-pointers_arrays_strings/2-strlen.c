6f556b1d679f2de1e4b5e7417768f20f5d556a25
Gzoref <gzoref@gmail.com>
2019-10-01 10:15:32 -0400

    swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char type
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+	return (a);
+}
