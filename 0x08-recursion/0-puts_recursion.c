5b2d05e425dfc5d006d9637494a6a6e1a0b8c55d
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-10-09 12:34:23 -0400

    prints a string using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _put_recursion - ecursion function to print string
+ * _puts_recursion - ecursion function to print string
  * @s: pointer of char type to input string
  * Return: void
  */
