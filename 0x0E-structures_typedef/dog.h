f0a8b60abad9a0b124475e4ad7a63d3d3f5c00be
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-10-21 12:21:09 -0400

    added all prototypes to header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,8 +5,6 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
- * Description: Longer description
  */
 
 typedef struct dog
@@ -20,5 +18,6 @@ int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
