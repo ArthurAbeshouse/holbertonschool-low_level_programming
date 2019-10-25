db78777495285ec2eeda9e8a8d8de36be9de5f41
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-10-25 13:29:11 -0400

    fixed betty checks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,12 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(addlist, n);
 
 	if (n == 0)
-                return (0);
+		return (0);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(addlist, unsigned int);
 
 	va_end(addlist);
-
-	return(sum);
+	return (sum);
 }
