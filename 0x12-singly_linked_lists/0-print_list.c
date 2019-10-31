8b971106346b9688224da14e530ae5a2faa0e757
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-10-31 12:21:44 -0400

    minor fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,14 +7,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+
+	while (h)
 	{
-		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		i++;
 		h = h->next;
 	}
