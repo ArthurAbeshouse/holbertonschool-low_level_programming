47d2add4c029f8b9e0229696ae1d607f15986848
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-05 13:33:36 -0500

    fixed missing checks

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 /**
- * free_list - frees a listint_t list
+ * free_listint - frees a listint_t list
  * @head: list to free
  */
 
 void free_listint(listint_t *head)
 {
-	while (head)
-	{
-		head = head->next;
-		free(head);
-	}
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
 }
