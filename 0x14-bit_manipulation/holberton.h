5b55155a65dfe418c9b85be3a81743c8dc4ae5c4
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-07 13:40:01 -0500

    added libraries to header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,10 @@
 #ifndef HEADER_H
 #define HEADER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -8,4 +13,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
+
 #endif
