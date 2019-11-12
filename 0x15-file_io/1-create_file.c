1bf07041b42cc24ad1709c23c35551d94b4e3bf0
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-12 13:25:04 -0500

    created if statment for if wr_stat is -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,18 +10,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int append_file, len, wr_stat;
 
-	if (filename == NULL)
+	if (filename)
 		return (-1);
 
 	append_file = open(filename, O_WRONLY | O_APPEND);
 	if (append_file == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content)
 		return (1);
 
 	for (len = 0; text_content[len]; len++)
 		;
 	wr_stat = write(append_file, text_content, len);
+	if (wr_stat == -1)
+		return (-1);
 	close(append_file);
 	return (1);
 }
