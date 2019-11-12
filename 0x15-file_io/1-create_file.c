60765ac89d5f8361208682b1ff0b76f252a5d07e
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-12 13:37:29 -0500

    added additional checks for wr_stat and close

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,27 +3,29 @@
  * create_file - appends text at the end of a file
  * @filename: name of file to append
  * @text_content: name of file written
- * Return: 1 id successful, -1 if otherwise
+ * Return: 1 if successful, -1 if otherwise
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int append_file, len, wr_stat;
 
-	if (filename)
+	if (!filename)
 		return (-1);
 
 	append_file = open(filename, O_WRONLY | O_APPEND);
 	if (append_file == -1)
 		return (-1);
-	if (text_content)
+	if (!text_content)
 		return (1);
 
 	for (len = 0; text_content[len]; len++)
 		;
 	wr_stat = write(append_file, text_content, len);
-	if (wr_stat == -1)
+	if (wr_stat == -1 || wr_stat != len)
 		return (-1);
 	close(append_file);
+	if (close == -1)
+		return (-1);
 	return (1);
 }
