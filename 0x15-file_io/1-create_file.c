47d56f55688bc423356c5443a9410433dbe57bb0
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-12 14:12:36 -0500

    now it works

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	append_file = open(filename, O_WRONLY | O_APPEND);
+	append_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (append_file == -1)
 		return (-1);
 	if (!text_content)
