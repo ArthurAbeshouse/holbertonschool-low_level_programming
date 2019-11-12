0133e1f7a1ded5ded21d9d9bd03297cb60101bfc
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-12 17:34:15 -0500

    C program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,47 +2,48 @@
 /**
  * main - C program that copies the content of a file to another file
  * @argc: size of array argv and number of command line arguments
- * @av: An  array  of size argc
+ * @argv: An  array  of size argc
  * Return: 0 if successful
  */
 
-int main(int argc, char *av[])
+int main(int argc, char *argv[])
 {
-	int file1, file2, fileread, filewrite;
+	int file1, file2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	file1 = open(av[1], O_RDONLY);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file2  = open(av[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((fileread = read(file1, buffer, 1024)) > 0)
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((filecheck = read(file1, buffer, 1024)) > 0)
 	{
-		if (file2 == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		if (filecheck == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		filecheck = write(file2, buffer, filecheck);
+		if (filecheck == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	filewrite = write(file2, buffer, fileread);
-	if (filewrite == -1)
+	if (filecheck == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (close(file1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
 	if (close(file2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
 	return (0);
 }
