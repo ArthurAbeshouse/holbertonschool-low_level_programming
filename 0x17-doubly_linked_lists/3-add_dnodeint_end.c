7521d11557dd5b52db1648ca9144ab290a7e3796
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-12-13 15:50:20 -0500

    frees a doubly linked list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a node at the end
+ * @head: head of the node
+ * @n: the new node element
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+        dlistint_t *new, *last;
+
+        if (head == NULL)
+                return (NULL);
+
+        new = malloc(sizeof(dlistint_t));
+        if (new == NULL)
+                return (NULL);
+
+        new->n = n;
+	new->next = NULL;
+        if (*head == NULL)
+        {
+                new->prev = NULL
+                *head = new;
+                return (new);
+        }
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+	return (new);
+}
