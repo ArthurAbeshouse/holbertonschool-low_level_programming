c037566f35d4dfe7b7334e0239f4a466b851291e
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-12-15 13:09:00 -0500

    reomved unnecessary ';'

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,14 +5,12 @@
  * @h: head of the node
  * Return: returns the amount of nodes
  */
-
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
 	if (h == NULL)
 		return (nodes);
-
 	while (h != NULL)
 	{
 		h = h->next
