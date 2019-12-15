3c2cdbbcfeae0614264aae10526001bcf1eec184
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-12-15 13:20:21 -0500

    fixed indents

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,23 +9,23 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *new, *last;
+	dlistint_t *new, *last;
 
-        if (head == NULL)
-                return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-        new = malloc(sizeof(dlistint_t));
-        if (new == NULL)
-                return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-        new->n = n;
+	new->n = n;
 	new->next = NULL;
-        if (*head == NULL)
-        {
-                new->prev = NULL
-                *head = new;
-                return (new);
-        }
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
