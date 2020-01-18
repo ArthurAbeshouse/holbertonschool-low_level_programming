d154b4b96473e5947521c5decd178f16a45344b3
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-01-18 16:00:17 -0500

    <= instead of ==

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,16 +2,14 @@
 
 /**
  * hash_table_create - creates a hash table
- *
  * @size: size of the hash table
- *
  * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
