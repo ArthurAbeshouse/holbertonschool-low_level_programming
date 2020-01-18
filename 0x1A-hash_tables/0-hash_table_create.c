5b11bfa6c6fda2806399585e172c30911fe80f9b
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-01-18 15:50:44 -0500

    switched back to malloc from calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int index = 0;
 
-	if (size == NULL)
-		return (NULL);
-
-	table = calloc(1, sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
-
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (!size)
 	{
-		free(table);
 		return (NULL);
 	}
-	return (table);
+	else
+	{
+		table->size = size;
+		table->array = malloc(size * sizeof(hash_node_t *));
+		if (!table->array)
+		{
+			free(table);
+			return (NULL);
+		}
+		else
+		{
+			for (index = 0; index < size; index++)
+				table->array[index] = NULL;
+			return (table);
+		}
+	}
 }
