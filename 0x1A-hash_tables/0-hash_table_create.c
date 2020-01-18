1776be8462ba9680c3c165caa5c9f28950700ca3
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-01-18 15:57:14 -0500

    added extra *'s

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,9 @@
 
 /**
  * hash_table_create - creates a hash table
+ *
  * @size: size of the hash table
+ *
  * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -10,16 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 
 	if (size == 0)
-	  return (NULL);
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-	  return (NULL);
+		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-	  {
-	    free(table);
-	    return (NULL);
-	  }
+	{
+		free(table);
+		return (NULL);
+	}
 	return (table);
 }
