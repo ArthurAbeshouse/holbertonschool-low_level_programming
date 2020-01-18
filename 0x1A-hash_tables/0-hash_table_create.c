b98145dcfc46f7f1789877006fe6a1ea534048b8
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-18 20:54:03 +0000

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,28 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	unsigned long int index = 0;
+	hash_table_t *table = NULL;
 
+	if (size == 0)
+	  return (NULL);
 	table = malloc(sizeof(hash_table_t));
-	if (!size)
-	{
-		return (NULL);
-	}
-	else
-	{
-		table->size = size;
-		table->array = malloc(size * sizeof(hash_node_t *));
-		if (!table->array)
-		{
-			free(table);
-			return (NULL);
-		}
-		else
-		{
-			for (index = 0; index < size; index++)
-				table->array[index] = NULL;
-			return (table);
-		}
-	}
+	if (table == NULL)
+	  return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	  {
+	    free(table);
+	    return (NULL);
+	  }
+	return (table);
 }
