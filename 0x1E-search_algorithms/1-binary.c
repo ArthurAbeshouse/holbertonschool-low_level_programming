4c1b1249dd724acd03d1da43bede2d75b9247af2
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-04-16 17:22:20 -0400

    fixed betty errors

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 
 		half = (left + right) / 2;
 		if (array[half] == value)
-			return half;
+			return (half);
 		else if (array[half] < value)
 			left = half + 1;
 		else
